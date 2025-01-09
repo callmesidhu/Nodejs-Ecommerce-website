98a0363f66f384d3e86536762a45dab4c07de6ec
S SIDHARTH <sidharthpunalur@gmail.com>
2025-01-09 08:44:09 +0530

    data from form to console

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var connectDB = require('./config/dbconnection');
 var fileUpload = require('express-fileupload');
+const cors = require('cors');
 
 // Connect to MongoDB
 connectDB();
@@ -13,56 +13,47 @@ connectDB();
 var homeRouter = require('./routes/home');
 var adminRouter = require('./routes/admin');
 var cartRouter = require('./routes/cart');
-var errorRouter = require('./routes/error');
 var productsRouter = require('./routes/products');
 var accountsRouter = require('./routes/accounts');
-var connectDB = require('./config/dbconnection'); 
+var errorRouter = require('./routes/error');
 
-
-
-var hbs = require('hbs'); 
-
+var hbs = require('hbs');
 
 var app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs'); // Set hbs as the view engine
 
-
-// To parse URL-encoded bodies (sent by HTML forms)
-app.use(express.urlencoded({ extended: true }));
-
-// To parse JSON bodies
-app.use(express.json());
+// Middleware
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data
+app.use(express.json()); // Parse JSON data
 app.use(fileUpload());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-
+// Routes
 app.use('/', homeRouter);
 app.use('/admin', adminRouter);
 app.use('/cart', cartRouter);
 app.use('/products', productsRouter);
 app.use('/account', accountsRouter);
-app.use('/*',errorRouter);
+app.use('/*', errorRouter); // Catch-all route for undefined routes
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// Error handler
+app.use(function (err, req, res, next) {
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/accounts.js
+++ routes/accounts.js
@@ -6,45 +6,19 @@ const { body, validationResult } = require('express-validator');
 
 router.get('/', function(req, res, next) {
        res.render('account');
-       });          
-
-router.post('/login', function(req, res, next) {});
-
-// Register route
-router.post(
-       '/register',
-       [
-         body('username').notEmpty().withMessage('Username is required'),
-         body('email').isEmail().withMessage('Invalid email format'),
-         body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-       ],
-       async (req, res, next) => {
-         const errors = validationResult(req);
-         if (!errors.isEmpty()) {
-           return res.status(400).json({ errors: errors.array() });
-         }
-     
-         const { username, email, password } = req.body;
-     
-         try {
-           // Check if the user already exists
-           const existingUser = users.find(user => user.email === email);
-           if (existingUser) {
-             return res.status(400).json({ message: 'Email is already registered' });
-           }
-     
-           // Hash the password
-           const hashedPassword = await bcrypt.hash(password, 10);
-     
-           // Save the user
-           const newUser = { username, email, password: hashedPassword };
-           users.push(newUser); // Replace with database insertion logic
-     
-           res.status(201).json({ message: 'User registered successfully' });
-         } catch (error) {
-           next(error); // Pass error to error-handling middleware
-         }
-       }
-     );
+       });    
+             
+  router.post('/register', async (req, res) => {
+        const { username, email, password } = req.body;
+      
+        console.log(req.body);
+      });
+      
+      // Login route
+router.post('/login', async (req, res) => {
+        const { email, password } = req.body;
+        console.log(req.body);
+      });
+      
 
 module.exports = router;
