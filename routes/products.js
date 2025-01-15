0b332cf9189fcefd49987898fd0350651188406f
S SIDHARTH <sidharthpunalur@gmail.com>
2025-01-16 01:01:14 +0530

    product section work completed

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,14 @@
 var express = require('express');
+const Product = require('../models/Product');
 var router = express.Router();
 
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
         let userDetails = req.session.user;
-        let userStatus = req.session.userlogin;
-       res.render('products');
+       let userStatus = req.session.userlogin;
+       const products = await Product.find().sort({ createdAt: -1 }); // -1 for descending order
+       
+       res.render('products', { products , userDetails , userStatus });
        }); 
        
 router.get('/details', function(req, res, next) {
